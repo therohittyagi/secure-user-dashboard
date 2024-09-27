73015d07965a751197bf4d58941c25a3612cf47f
Rohit Tyagi <pawan@odwen.co.in>
2024-09-27 12:39:21 +0530

    signin && signup && header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute top-0 w-full px-8 py-4 bg-gradient-to-b from-black z-10">
+      <h1 className="font-bold text-3xl text-fuchsia-500">
+        Secure User Dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
